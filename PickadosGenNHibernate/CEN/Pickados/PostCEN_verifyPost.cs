689d9d48827e5e24c78ffb1f45ac77a6e7903b92
Jose Antonio Rubio <josearuol@gmail.com>
2017-01-15 13:05:40 +0100

    Implementing custom method verifyPost

--- PickadosGenNHibernate/CEN/Pickados/PostCEN_verifyPost.cs
+++ PickadosGenNHibernate/CEN/Pickados/PostCEN_verifyPost.cs
@@ -9,6 +9,7 @@ using NHibernate.Exceptions;
 using PickadosGenNHibernate.Exceptions;
 using PickadosGenNHibernate.EN.Pickados;
 using PickadosGenNHibernate.CAD.Pickados;
+using System.Linq;
 
 
 /*PROTECTED REGION ID(usingPickadosGenNHibernate.CEN.Pickados_Post_verifyPost) ENABLED START*/
@@ -21,11 +22,42 @@ public partial class PostCEN
 {
 public void VerifyPost (int p_oid)
 {
-        /*PROTECTED REGION ID(PickadosGenNHibernate.CEN.Pickados_Post_verifyPost) ENABLED START*/
+            /*PROTECTED REGION ID(PickadosGenNHibernate.CEN.Pickados_Post_verifyPost) ENABLED START*/
 
-        // Write here your custom code...
+            // Write here your custom code...
+            PostCAD postCAD = new PostCAD();
+            PostCEN postCEN = new PostCEN(postCAD);
+            PostEN postEN = postCEN.GetByID(p_oid);
 
-        throw new NotImplementedException ("Method VerifyPost() not yet implemented.");
+            if (postEN != null) {
+
+                //Comprobamos que el pick o los picks hayan finalizado
+                if(postEN.Pick!=null && postEN.Pick.Any())
+                {
+                    Boolean finished = true;
+                    foreach (PickEN pick in postEN.Pick)
+                    {
+                        if (pick.PickResult.Equals(Enumerated.Pickados.PickResultEnum.unfinished) ||
+                           pick.PickResult.Equals(Enumerated.Pickados.PickResultEnum.unstarted)) {
+                            finished = false;
+                        }
+                    }
+
+                    if (finished) {
+
+                        TipsterCAD tipsterCAD = new TipsterCAD();
+                        TipsterCEN tipsterCEN = new TipsterCEN(tipsterCAD);
+                        TipsterEN tipsterEN = tipsterCEN.GetByID(postEN.Tipster.Id);
+
+                        //Comprueba si hay stats creadas, si no, crea para este mes
+
+
+                        //Si hay stats, actualiza la existente de este mes
+                    }
+                }
+
+
+            }
 
         /*PROTECTED REGION END*/
 }
