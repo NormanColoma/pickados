bc1d75874f04aa90c15bc6eaccb90ce8571a850a
Montoro <vimon_92@hotmail.com>
2017-03-05 20:41:38 +0100

    added two methos which receives only one follower or unfollow instead of a list

--- PickadosGen/PickadosGenPickadosRESTAzure/Controllers/PickController.cs
+++ PickadosGen/PickadosGenPickadosRESTAzure/Controllers/PickController.cs
@@ -30,6 +30,56 @@ public class PickController : BasicController
 
 
 
+// ReadAll Generado a partir del NavigationalOperation
+[HttpGet]
+
+[Route ("~/api/Pick/GetAllPicks")]
+public HttpResponseMessage GetAllPicks ()
+{
+        // CAD, CEN, EN, returnValue
+        PickRESTCAD pickRESTCAD = null;
+        PickCEN pickCEN = null;
+
+        List<PickEN> pickEN = null;
+        List<PickDTOA> returnValue = null;
+
+        try
+        {
+                SessionInitializeWithoutTransaction ();
+                pickRESTCAD = new PickRESTCAD (session);
+                pickCEN = new PickCEN (pickRESTCAD);
+
+                // Data
+                // TODO: paginación
+
+                pickEN = pickCEN.GetAllPicks (0, -1).ToList ();
+
+                // Convert return
+                if (pickEN != null) {
+                        returnValue = new List<PickDTOA>();
+                        foreach (PickEN entry in pickEN)
+                                returnValue.Add (PickAssembler.Convert (entry, session));
+                }
+        }
+
+        catch (Exception e)
+        {
+                if (e.GetType () == typeof(HttpResponseException)) throw e;
+                else if (e.GetType () == typeof(PickadosGenNHibernate.Exceptions.ModelException) || e.GetType () == typeof(PickadosGenNHibernate.Exceptions.DataLayerException)) throw new HttpResponseException (HttpStatusCode.BadRequest);
+                else throw new HttpResponseException (HttpStatusCode.InternalServerError);
+        }
+        finally
+        {
+                SessionClose ();
+        }
+
+        // Return 204 - Empty
+        if (returnValue == null || returnValue.Count == 0)
+                return this.Request.CreateResponse (HttpStatusCode.NoContent);
+        // Return 200 - OK
+        else return this.Request.CreateResponse (HttpStatusCode.OK, returnValue);
+}
+
 
 
 
--- PickadosGen/PickadosGenPickadosRESTAzure/Controllers/PostController.cs
+++ PickadosGen/PickadosGenPickadosRESTAzure/Controllers/PostController.cs
@@ -38,6 +38,52 @@ public class PostController : BasicController
 
 
 
+[HttpGet]
+// [Route("{idPost}", Name="GetOIDPost")]
+
+[Route ("~/api/Post/{idPost}")]
+
+public HttpResponseMessage GetPostById (int idPost)
+{
+        // CAD, CEN, EN, returnValue
+        PostRESTCAD postRESTCAD = null;
+        PostCEN postCEN = null;
+        PostEN postEN = null;
+        PostDTOA returnValue = null;
+
+        try
+        {
+                SessionInitializeWithoutTransaction ();
+                postRESTCAD = new PostRESTCAD (session);
+                postCEN = new PostCEN (postRESTCAD);
+
+                // Data
+                postEN = postCEN.GetPostById (idPost);
+
+                // Convert return
+                if (postEN != null) {
+                        returnValue = PostAssembler.Convert (postEN, session);
+                }
+        }
+
+        catch (Exception e)
+        {
+                if (e.GetType () == typeof(HttpResponseException)) throw e;
+                else if (e.GetType () == typeof(PickadosGenNHibernate.Exceptions.ModelException) || e.GetType () == typeof(PickadosGenNHibernate.Exceptions.DataLayerException)) throw new HttpResponseException (HttpStatusCode.BadRequest);
+                else throw new HttpResponseException (HttpStatusCode.InternalServerError);
+        }
+        finally
+        {
+                SessionClose ();
+        }
+
+        // Return 404 - Not found
+        if (returnValue == null)
+                return this.Request.CreateResponse (HttpStatusCode.NotFound);
+        // Return 200 - OK
+        else return this.Request.CreateResponse (HttpStatusCode.OK, returnValue);
+}
+
 
 
 // No pasa el slEnables: findPostsByTipster
@@ -110,6 +156,85 @@ public HttpResponseMessage FindPostsByTipster (int id)
 
 
 
+
+
+
+[HttpPost]
+
+[Route ("~/api/Post/VerifyPost")]
+
+public HttpResponseMessage VerifyPost (int p_oid)
+{
+        // CP, returnValue
+        PostCP postCP = null;
+
+
+        try
+        {
+                SessionInitializeTransaction ();
+                postCP = new PostCP (session);
+
+                // Operation
+                postCP.VerifyPost (p_oid);
+                SessionCommit ();
+        }
+
+        catch (Exception e)
+        {
+                SessionRollBack ();
+
+                if (e.GetType () == typeof(HttpResponseException)) throw e;
+                else if (e.GetType () == typeof(PickadosGenNHibernate.Exceptions.ModelException) || e.GetType () == typeof(PickadosGenNHibernate.Exceptions.DataLayerException)) throw new HttpResponseException (HttpStatusCode.BadRequest);
+                else throw new HttpResponseException (HttpStatusCode.InternalServerError);
+        }
+        finally
+        {
+                SessionClose ();
+        }
+
+        // Return 200 - OK
+        return this.Request.CreateResponse (HttpStatusCode.OK);
+}
+
+
+
+[HttpPost]
+
+[Route ("~/api/Post/PublishPost")]
+
+public HttpResponseMessage PublishPost (Nullable<DateTime> p_created_at, Nullable<DateTime> p_modified_at, double p_stake, string p_description, bool p_private, System.Collections.Generic.IList<int> p_pick, int p_tipster, PickadosGenNHibernate.Enumerated.Pickados.PickResultEnum p_postresult)
+{
+        // CP, returnValue
+        PostCP postCP = null;
+
+        int returnValue;
+
+        try
+        {
+                SessionInitializeTransaction ();
+                postCP = new PostCP (session);
+
+                // Operation
+                returnValue = postCP.PublishPost (p_created_at, p_modified_at, p_stake, p_description, p_private, p_pick, p_tipster, p_postresult);
+                SessionCommit ();
+        }
+
+        catch (Exception e)
+        {
+                SessionRollBack ();
+
+                if (e.GetType () == typeof(HttpResponseException)) throw e;
+                else if (e.GetType () == typeof(PickadosGenNHibernate.Exceptions.ModelException) || e.GetType () == typeof(PickadosGenNHibernate.Exceptions.DataLayerException)) throw new HttpResponseException (HttpStatusCode.BadRequest);
+                else throw new HttpResponseException (HttpStatusCode.InternalServerError);
+        }
+        finally
+        {
+                SessionClose ();
+        }
+
+        // Return 200 - OK
+        return this.Request.CreateResponse (HttpStatusCode.OK, returnValue);
+}
 
 
 
