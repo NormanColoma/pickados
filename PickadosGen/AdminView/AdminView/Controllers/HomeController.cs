39934b486ae2ed446d967ad3b9eba6bbe0c45295
Montoro <vimon_92@hotmail.com>
2017-04-02 23:12:10 +0200

    added partial view of teams/players administration

--- PickadosGen/AdminView/AdminView/Controllers/HomeController.cs
+++ PickadosGen/AdminView/AdminView/Controllers/HomeController.cs
@@ -12,5 +12,20 @@ namespace AdminView.Controllers
         {
             return View();
         }
+
+        public ActionResult Teams()
+        {
+            return View();
+        }
+
+        public ActionResult DetailsTeam(int id = 0)
+        {
+            return View(id);
+        }
+
+        public ActionResult DetailsPlayer(int id = 0)
+        {
+            return View(id);
+        }
     }
 }
