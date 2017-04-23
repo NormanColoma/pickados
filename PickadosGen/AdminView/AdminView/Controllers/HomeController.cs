26b77b924a16e145c616a390282246c8f2fe30bd
Montoro <vimon_92@hotmail.com>
2017-04-23 15:32:40 +0200

    añadido detalles para el equipo

--- PickadosGen/AdminView/AdminView/Controllers/HomeController.cs
+++ PickadosGen/AdminView/AdminView/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PickadosGenNHibernate.CEN.Pickados;
+using PickadosGenNHibernate.EN.Pickados;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,12 +21,25 @@ namespace AdminView.Controllers
 
         public ActionResult Teams()
         {
+            TeamCEN teams = new TeamCEN();
+            var totalTeams = teams.GetAllTeams(1, 2000).OrderBy(p => p.Country);
+            ViewBag.allTeams = totalTeams;
             return View();
         }
 
         public ActionResult DetailsTeam(int id = 0)
         {
-            return View(id);
+            if (id != 0)
+            {
+                TeamCEN team = new TeamCEN();
+                PlayerCEN player = new PlayerCEN();
+                var totalTeams = team.GetAllTeams(1, 2000).OrderBy(p => p.Country);
+                ViewBag.allTeams = totalTeams;
+                TeamEN teamToSearch = team.GetTeamById(id);
+                ViewBag.team = teamToSearch;
+                ViewBag.players = player.GetPlayersByClubTeam(teamToSearch.Name, 1, 20000);
+            }
+            return View();
         }
 
         public ActionResult DetailsPlayer(int id = 0)
