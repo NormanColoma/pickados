69cb8e59b16b84e358da664ff0771f462d8c9f7c
Montoro <vimon_92@hotmail.com>
2017-06-15 17:32:34 +0200

    creada sección de partidos

--- PickadosGen/AdminView/AdminView/Controllers/MatchesController.cs
+++ PickadosGen/AdminView/AdminView/Controllers/MatchesController.cs
@@ -18,11 +18,21 @@ namespace AdminView.Controllers
 
         public ActionResult Matches(int idRound = 0, int idSeason = 0, int idCompetition = 0, string countryName = "", int idSport = 0, string typeCompetition = "")
         {
+            CompetitionCEN competis = new CompetitionCEN();
+            SportCEN sports = new SportCEN();
+            SeasonCEN seasons = new SeasonCEN();
+            RoundCEN rounds = new RoundCEN();
             Event_CEN events = new Event_CEN();
             List<Event_EN> allEvents = events.GetEventsByRound(idRound).ToList();
             List<MatchEN> allMatches = new List<MatchEN>();
             MatchCEN matches = new MatchCEN();
-            foreach(Event_EN ev in allEvents)
+            ViewBag.country = countryName;
+            ViewBag.sport = sports.GetSportById(idSport);
+            ViewBag.competition = competis.GetCompetitionById(idCompetition);
+            ViewBag.season = seasons.GetSeasonByID(idSeason);
+            ViewBag.typeCompetition = typeCompetition;
+            ViewBag.round = rounds.ReadOID(idRound);
+            foreach (Event_EN ev in allEvents)
             {
                 allMatches.Add(matches.GetMatchById(ev.Id));
             }
