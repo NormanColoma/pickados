528e1de734a70770d265909824fc2d3121bb058f
Montoro <vimon_92@hotmail.com>
2017-07-04 23:47:07 +0200

    creados detalles de post y pick

---
+++ PickadosGen/AdminView/AdminView/Controllers/PickController.cs
@@ -0,0 +1,26 @@
+﻿using PickadosGenNHibernate.CEN.Pickados;
+using PickadosGenNHibernate.EN.Pickados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminView.Controllers
+{
+    public class PickController : Controller
+    {
+        // GET: Pick
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DetailsPick(int idPick)
+        {
+            PickCEN picks = new PickCEN();
+            PickEN pick = picks.GetPickById(idPick);
+            return PartialView("DetailsPick", pick);
+        }
+    }
+}
---
+++ PickadosGen/AdminView/AdminView/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using PickadosGenNHibernate.CEN.Pickados;
+using PickadosGenNHibernate.EN.Pickados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminView.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PostDetails(int post=0)
+        {
+            PostCEN posts = new PostCEN();
+            PickCEN picks = new PickCEN();
+            PostEN postTotal = posts.GetPostById(post);
+            ViewBag.picks = picks.GetPicksByPost(post);
+            return View(postTotal);
+        }
+    }
+}
